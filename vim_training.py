3a95d913c4d1579a7ed17ba6af4e3002f6d13e82
angelalonso <alonsofonseca.angel@gmail.com>
2017-01-21 00:39:28 +0100

    some TO DOs

--- vim_training.py
+++ vim_training.py
@@ -8,21 +8,26 @@ import signal
 import subprocess
 import time
 
-VIM_BIN = "/usr/bin/vim"
-TIMER = 15
 
 
-def main():
+def process_controller(binary, timer):
     """
     Main function
     """
 
+    # TODO: Show timer on pyqt
+
     proc = subprocess.Popen(VIM_BIN)
     print "PID:", proc.pid
     # print "Return code:", proc.wait()
-    time.sleep(TIMER)
+    time.sleep(timer)
 
     os.kill(proc.pid, signal.SIGTERM)
 
 if __name__ == "__main__":
-    main()
+    VIM_BIN = "/usr/bin/vim"
+    TIMER = 5
+
+    # Show configuration main screen, take vars from there
+
+    process_controller(VIM_BIN, TIMER)
