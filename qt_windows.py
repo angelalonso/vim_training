c96e81f472fe65b63f1cb70befddc6707cf4329f
angelalonso <alonsofonseca.angel@gmail.com>
2017-01-21 16:34:13 +0100

    time controller working, not pretty

--- qt_windows.py
+++ qt_windows.py
@@ -3,7 +3,6 @@
 
 """
 Manages the pyQt windows needed
-TO BE ADAPTED FOR THIS PROJECT!
 """
 
 
@@ -14,14 +13,14 @@ from PyQt4 import QtGui, QtCore
 class CounterWindow(QtGui.QWidget):
     """
     window class for PyQt4 countdown timer
+    Based on
+    http://stackoverflow.com/questions/12661211/cant-seem-to-get-pyqt-countdown-timer-to-work#12661563
     """
-    def __init__(self, parent=None):
+    def __init__(self, timerinit, parent=None):
         QtGui.QWidget.__init__(self, parent)
 
-        self.inicio = 5
-
         self.timer = QtCore.QTimer()
-        text = "%d:%02d" % (self.inicio / 60, self.inicio % 60)
+        text = "%d:%02d" % (timerinit / 60, timerinit % 60)
 
         self.timerlabel = QtGui.QLabel()
         self.timerlabel.move(20, 20)
@@ -33,27 +32,27 @@ class CounterWindow(QtGui.QWidget):
         self.gridLayout.addWidget(self.timerlabel, 1, 1, 1, 1)
         self.setLayout(self.gridLayout)
 
-        self.startTimerDisplay()
+        self.startTimerDisplay(timerinit)
         self.timer.timeout.connect(self.updateTimerDisplay)
 
-    def startTimerDisplay(self):
+    def startTimerDisplay(self, timerinit):
         """ set the countdown value and start the timer """
-        self.inicio = 5
+        self.init = timerinit
         self.timer.start(1000)
 
     def updateTimerDisplay(self):
         """ count down one second, set the text,
         and check if the timer should stop """
-        self.inicio -= 1
-        text = "%d:%02d" % (self.inicio/60, self.inicio % 60)
+        self.init -= 1
+        text = "%d:%02d" % (self.init / 60, self.init % 60)
         self.timerlabel.setText(text)
-        if self.inicio == 0:
+        if self.init == 0:
             self.timer.stop()
+            self.close()
 
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
-    mainwin = CounterWindow()
-    #mainwin.updateTimerDisplay()
+    mainwin = CounterWindow(5)
     mainwin.show()
     sys.exit(app.exec_())
--- vim_training.py
+++ vim_training.py
@@ -8,8 +8,17 @@ Controls all parts involved on the VIM Training
 import os
 import signal
 import subprocess
+import sys
 import time
+from PyQt4 import QtGui
+import qt_windows
 
+
+def timer_controller(timer):
+    app = QtGui.QApplication(sys.argv)
+    mainwin = qt_windows.CounterWindow(timer)
+    mainwin.show()
+    sys.exit(app.exec_())
 
 
 def process_controller(binary, timer):
@@ -17,19 +26,19 @@ def process_controller(binary, timer):
     Main function
     """
 
-    # TODO: Show timer on pyqt
-
-    proc = subprocess.Popen(VIM_BIN)
+    proc = subprocess.Popen(binary)
     print "PID:", proc.pid
     # print "Return code:", proc.wait()
-    time.sleep(timer)
+    timer_controller(timer)
 
     os.kill(proc.pid, signal.SIGTERM)
 
 if __name__ == "__main__":
     VIM_BIN = "/usr/bin/vim"
-    TIMER = 5
+    TIMER = 6
 
-    # Show configuration main screen, take vars from there
+    # TODO:
+    # If no configuration or parameter "-config":
+    #   Show configuration main screen, take vars from there
 
     process_controller(VIM_BIN, TIMER)
